01ffb820aa3b242fd92dcd0529f54d7274cbd014
Nadarsha111 A <nadarsha565@gmail.com>
2024-04-08 14:47:17 +0000

    completed with multer

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer');
+
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload=multer({dest:'uploads/'})
 
+app.post('/api/fileanalyse',upload.single("upfile"),(req,res)=>{
+  const {originalname,mimetype,size}=req.file
+  
+  res.send({
+    name: originalname,
+    type: mimetype,
+    size
+})
 
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
